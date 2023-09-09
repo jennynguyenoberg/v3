f8c9b8fa5dc167b0a26754cbd2ccd7b1dde9db99
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-09 08:51:26 +0200

    Change margin and make responsive

--- app/hero/page.js
+++ app/hero/page.js
@@ -4,27 +4,34 @@ import Link from 'next/link';
 export default function Hero() {
   return (
     <div className={styles.mainContainer}>
-      <h5>Site is under maintenance</h5>
-      <div className={styles.header}>
-        <h1 className={styles.title}>Jenny Nguyen Öberg</h1>
-      </div>
-      <div className={styles.columnContainer}>
-        <div className={styles.column1}>
-          <p className={styles.descriptionText}>
-            Designer & Web Developer student based in beautiful Stockholm,
-            Sweden. Creative at heart with a passion for the details.
-          </p>
+      <div className={styles.textContainer}>
+        <h5>Site is under maintenance</h5>
+
+        <div className={styles.header}>
+          <h1 className={styles.title}>Jenny Nguyen Öberg</h1>
         </div>
-        <div className={styles.column2}>
-          <div className={styles.category}>
-            <Link href="/">Design</Link>
+
+        <div className={styles.columnContainer}>
+          <div className={styles.column1}>
+            <p className={styles.descriptionText}>
+              Designer & Web Developer student based in beautiful Stockholm,
+              Sweden. Creative at heart with a passion for the details.
+            </p>
+          </div>
+
+          <div className={styles.column2}>
+            <div className={styles.category}>
+              <Link href="/">Design</Link>
+            </div>
+          </div>
+          
+          <div className={styles.column3}>
+            <div className={styles.category}>
+              <Link href="/">&#60;&#47;Code&#62;</Link>
+            </div>
           </div>
         </div>
-        <div className={styles.column3}>
-          <div className={styles.category}>
-            <Link href="/">&#60;&#47;Code&#62;</Link>
-          </div>
-        </div>
+
       </div>
     </div>
   );
