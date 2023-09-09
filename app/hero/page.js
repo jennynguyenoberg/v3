3613d00237ac0a1c29ccc85faccffe744630ab9d
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-09 17:37:06 +0200

    Add styling to hero section

--- app/hero/page.js
+++ app/hero/page.js
@@ -1,37 +1,20 @@
 import styles from './hero.module.css';
 import Link from 'next/link';
+import LinkItem from '@/app/components/link/page'
 
 export default function Hero() {
   return (
     <div className={styles.mainContainer}>
       <div className={styles.textContainer}>
-        <h5>Site is under maintenance</h5>
-
+        <div className={styles.row}>
+        </div>
+        <div className={styles.rowWrapper}>
+          <span href="/">Designer & Developer</span>
+          <span>&#60;Site is under maintenance &#47;&#62;</span>
+        </div>
         <div className={styles.header}>
-          <h1 className={styles.title}>Jenny Nguyen Öberg</h1>
+          <h1 className={styles.title}>Jenny Nguyen Öberg —</h1>
         </div>
-
-        <div className={styles.columnContainer}>
-          <div className={styles.column1}>
-            <p className={styles.descriptionText}>
-              Designer & Web Developer student based in beautiful Stockholm,
-              Sweden. Creative at heart with a passion for the details.
-            </p>
-          </div>
-
-          <div className={styles.column2}>
-            <div className={styles.category}>
-              <Link href="/">Design</Link>
-            </div>
-          </div>
-          
-          <div className={styles.column3}>
-            <div className={styles.category}>
-              <Link href="/">&#60;&#47;Code&#62;</Link>
-            </div>
-          </div>
-        </div>
-
       </div>
     </div>
   );
