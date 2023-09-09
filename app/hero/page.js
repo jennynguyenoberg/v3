89e889c73501054f2df1ce35f46f4d5b87011e4e
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-09 17:48:13 +0200

    Clean code

--- app/hero/page.js
+++ app/hero/page.js
@@ -1,13 +1,10 @@
 import styles from './hero.module.css';
-import Link from 'next/link';
 import LinkItem from '@/app/components/link/page'
 
 export default function Hero() {
   return (
     <div className={styles.mainContainer}>
       <div className={styles.textContainer}>
-        <div className={styles.row}>
-        </div>
         <div className={styles.rowWrapper}>
           <span href="/">Designer & Developer</span>
           <span>&#60;Site is under maintenance &#47;&#62;</span>
