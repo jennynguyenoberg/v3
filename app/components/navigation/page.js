eb394e331a88f17ca6d107d08b51357cfb480b60
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-09 10:30:14 +0200

    Second try navbar

--- app/components/navigation/page.js
+++ app/components/navigation/page.js
@@ -1,11 +1,11 @@
 'use client';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from './navigation.module.css';
 import classNames from 'classnames';
 import Image from 'next/image';
-import { useState, useEffect } from 'react';
 
-export default function Sidebar() {
+export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const [shouldCloseMenu, setShouldCloseMenu] = useState(false);
 
@@ -28,16 +28,14 @@ export default function Sidebar() {
   };
 
   const toggleMenu = () => {
-    if (isOpen) {
-      setIsOpen(false);
-      setShouldCloseMenu(false);
-    } else {
-      setIsOpen(true);
-      setShouldCloseMenu(true);
-    }
+    setIsOpen(!isOpen);
+    setShouldCloseMenu(!isOpen);
   };
 
   useEffect(() => {
+    // Initialize isOpen to false when the component mounts
+    setIsOpen(false);
+
     const handleDocumentClick = (e) => {
       const menu = document.querySelector(`.${styles['menu-icon']}`);
       if (menu && !menu.contains(e.target)) {
@@ -56,11 +54,34 @@ export default function Sidebar() {
     document.addEventListener('click', handleDocumentClick);
     window.addEventListener('scroll', handleScroll);
 
+    // Remove event listeners when the component unmounts
     return () => {
       document.removeEventListener('click', handleDocumentClick);
       window.removeEventListener('scroll', handleScroll);
     };
   }, [shouldCloseMenu]);
+
+  useEffect(() => {
+    // Add a listener to check and set isOpen based on screen width
+    const handleResize = () => {
+      if (window.innerWidth < 992) {
+        setIsOpen(false);
+      } else {
+        setIsOpen(true);
+      }
+    };
+
+    // Initial check on component mount
+    handleResize();
+
+    // Add a listener for window resize events
+    window.addEventListener('resize', handleResize);
+
+    // Remove the resize event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -74,7 +95,7 @@ export default function Sidebar() {
           <div
             className={`${styles['menu-icon']} ${isOpen ? styles.open : ''}`}
             onClick={toggleMenu}
-            >
+          >
             <div className={styles.bar}></div>
             <div className={styles.bar}></div>
             <div className={styles.bar}></div>
@@ -85,23 +106,23 @@ export default function Sidebar() {
                 className={`${styles['navbar-items']} ${
                   isOpen ? styles.open : ''
                 }`}
-                >
+              >
                 {Object.entries(navItems).map(([path, { name }]) => (
                   <a
-                  key={path}
-                  href={path}
-                  className={classNames(styles.navigationItem)}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    // Smooth scroll to the section
-                    const sectionId = path.substring(1);
-                    const section = document.getElementById(sectionId);
-                    if (section) {
-                      section.scrollIntoView({
-                        behavior: 'smooth',
-                      });
-                    }
-                  }}
+                    key={path}
+                    href={path}
+                    className={classNames(styles.navigationItem)}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      // Smooth scroll to the section
+                      const sectionId = path.substring(1);
+                      const section = document.getElementById(sectionId);
+                      if (section) {
+                        section.scrollIntoView({
+                          behavior: 'smooth',
+                        });
+                      }
+                    }}
                   >
                     <span className={styles.linkName}>{name}</span>
                   </a>
