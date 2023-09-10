915a7b6c3118004b453a835b78d2b639a9121247
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-11 00:11:22 +0200

    Add hover effect on buttons + smooth transitions on sections

--- app/components/button/page.js
+++ app/components/button/page.js
@@ -3,8 +3,10 @@ import classNames from 'classnames';
 
 export default function Button({ children, className, ...props }) {
   return (
-    <button className={classNames(styles.container, className)} {...props}>
-      {children}
-    </button>
+    <div className={styles.main}>
+      <button className={classNames(styles.container, className)} {...props}>
+        {children}
+      </button>
+    </div>
   );
 }
