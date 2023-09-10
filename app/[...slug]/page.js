418e74ef0701837623af686d393a636c8c5fc083
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-10 11:35:15 +0200

    Clean up code 404-page

--- app/[...slug]/page.js
+++ app/[...slug]/page.js
@@ -5,14 +5,10 @@ export default function NotFoundPage() {
   return (
     <>
       <div className={styles.container}>
-        <h2 className={styles.subtitle}>Oops...</h2>
-        <h1 className={styles.title}>404</h1>
+        <span className={styles.subtitle}>Oops...</span>
+        <span className={styles.title}>404</span>
         <p>That page cannot be found.</p>
-        <div className={styles.link}>
-          <p className={styles.para}>
-            Go back to the <LinkItem href="/">Homepage</LinkItem>
-          </p>
-        </div>
+        <p>Go back to the <LinkItem href="/">Homepage</LinkItem></p>
       </div>
     </>
   );
