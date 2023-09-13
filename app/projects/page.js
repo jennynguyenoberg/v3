eab79dddfc0c2f2fe9f5af118e2b476d5135e0c9
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-13 22:04:26 +0200

    Add more objects in projects array + styling projects page

--- app/projects/page.js
+++ app/projects/page.js
@@ -8,17 +8,29 @@ import styles from "./projects.module.css";
 export default function Projects() {
   return (
     <div className={styles.container}>
-      <div className={styles.line}><Line /></div>
+      <div className={styles.line}>
+        <Line />
+      </div>
       <div className={styles.items}>
         {data.map((item) => (
           <div key={item.id} className={styles.projectItem}>
-            <div className={styles.heading}>
-              <Link href={`/projects/${item.id}`} className={styles.link}>
-                <SpanText>{item.heading}</SpanText>
-              </Link>
-            </div>
             <Link href={`/projects/${item.id}`} className={styles.link}>
-              <ParagraphText>{item.subheading}</ParagraphText>
+              <div className={styles.heading}>
+                <SpanText>{item.heading}</SpanText>
+              </div>
+            </Link>
+            <Link href={`/projects/${item.id}`} className={styles.link}>
+              <div className={styles.projectInfo}>
+                <div className={styles.columnDeliverables}>
+                  <ParagraphText>{item.deliverables}</ParagraphText>
+                </div>
+                <div className={styles.columnClient}>
+                  <ParagraphText>{item.client}</ParagraphText>
+                </div>
+                <div className={styles.columnYear}>
+                  <ParagraphText>{item.year}</ParagraphText>
+                </div>
+              </div>
             </Link>
           </div>
         ))}
--- app/utils/projectsdata.js
+++ app/utils/projectsdata.js
@@ -1,29 +1,96 @@
 const projectsData = [
   {
     id: 'project-one',
-    image: '/images/image1.jpg',
+    images: [
+      { 
+        url: '/images/image1.jpg', 
+        alt: 'Image 1 Alt Text' 
+      },
+      { 
+        url: '/images/image1.jpg',
+        alt: 'Image 2 Alt Text' 
+      },
+      { 
+        url: '/images/image1.jpg',
+        alt: 'Image 3 Alt Text' 
+      },
+    ],
     heading: 'Heading 1',
     subheading: 'Subheading 1',
+    role: 'Designer',
+    year: '2023',
+    deliverables: 'Development',
+    client: 'Bestclient.com',
+    specs: 'https://jennynguyenoberg.com',
+    site: 'https://jennynguyenoberg.com',
+    github: 'https://jennynguyenoberg.com',
+    brief: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Molestie ac feugiat sed lectus vestibulum. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur. Quis viverra nibh cras pulvinar mattis.'
   },
   {
     id: 'project-two',
-    image: '/images/image1.jpg',
+    images: [
+      { 
+        url: '/images/image1.jpg',
+        alt: 'Image 4 Alt Text' 
+      },
+      { 
+        url: '/images/image1.jpg',
+        alt: 'Image 5 Alt Text' 
+      },
+    ],
     heading: 'Heading 2',
     subheading: 'Subheading 2',
+    role: 'Designer',
+    year: '2023',
+    deliverables: 'Design & Development',
+    client: 'Bestclient.com',
+    specs: 'https://jennynguyenoberg.com',
+    site: 'https://jennynguyenoberg.com',
+    github: 'https://jennynguyenoberg.com',
+    brief: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Molestie ac feugiat sed lectus vestibulum. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur. Quis viverra nibh cras pulvinar mattis.'
   },
   {
     id: 'project-three',
-    image: '/images/image1.jpg',
+    images: [
+      { 
+        url: '/images/image1.jpg',
+        alt: 'Image 6 Alt Text' 
+      },
+    ],
     heading: 'Heading 3',
     subheading: 'Subheading 3',
+    role: 'Designer',
+    year: '2023',
+    deliverables: 'Packaging',
+    client: 'Bestclient.com',
+    specs: 'https://jennynguyenoberg.com',
+    site: 'https://jennynguyenoberg.com',
+    github: 'https://jennynguyenoberg.com',
+    brief: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Molestie ac feugiat sed lectus vestibulum. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur. Quis viverra nibh cras pulvinar mattis.'
   },
   {
     id: 'project-four',
-    image: '/images/image1.jpg',
+    images: [
+      { 
+        url: '/images/image1.jpg',
+        alt: 'Image 7 Alt Text' 
+      },
+      { 
+        url: '/images/image1.jpg',
+        alt: 'Image 8 Alt Text' 
+      },
+    ],
     heading: 'Heading 4',
     subheading: 'Subheading 4',
+    role: 'Designer',
+    year: '2023',
+    deliverables: 'Brand Identity, Packaging',
+    client: 'Bestclient.com',
+    specs: 'https://jennynguyenoberg.com',
+    site: 'https://jennynguyenoberg.com',
+    github: 'https://jennynguyenoberg.com',
+    brief: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Molestie ac feugiat sed lectus vestibulum. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur. Quis viverra nibh cras pulvinar mattis.'
   },
-
 ];
 
 export default projectsData;
