0500f2b23c6ae302d8deb55941fc7c4e65c3417e
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-13 19:05:57 +0200

    Rearrange projects structure + update styling

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,17 +1,17 @@
 import Link from "next/link";
 import data from "@/app/utils/projectsdata";
-import Image from "next/legacy/image";
+// import Image from "next/legacy/image";
 import Line from "../components/line/page";
 import SpanText from "../components/headings/spanText/page";
 import ParagraphText from "../components/headings/paragraphText/page";
-import styles from "@/app/sections/projects/projects.module.css";
+import styles from "./projects.module.css";
 
 export default function Projects() {
   return (
     <div className={styles.items}>
       {data.map((item) => (
         <div key={item.id} className={styles.projectItem}>
-          <Link href={`/projects/${item.id}`}>
+          {/* <Link href={`/projects/${item.id}`}>
             <div className={styles.link}>
               <Image
                 src={item.image}
@@ -22,16 +22,16 @@ export default function Projects() {
                 className={styles.projectImage}
               />
             </div>
-          </Link>
+          </Link> */}
           <div className={styles.heading}>
             <Link href={`/projects/${item.id}`} className={styles.link}>
               <SpanText>{item.heading}</SpanText>
             </Link>
           </div>
-          <Line />
           <Link href={`/projects/${item.id}`} className={styles.link}>
             <ParagraphText>{item.subheading}</ParagraphText>
           </Link>
+          <Line />
         </div>
       ))}
     </div>
--- app/sections/projects/page.js
+++ app/sections/projects/page.js
@@ -1,19 +1,49 @@
+import Link from "next/link";
+import data from "@/app/utils/projectsdata";
+import Image from "next/legacy/image";
+import Line from "@/app/components/line/page";
 import SpanText from "@/app/components/headings/spanText/page";
+import ParagraphText from "@/app/components/headings/paragraphText/page";
 import styles from "./projects.module.css";
 import SmallText from "@/app/components/headings/smallText/page";
-import ProjectList from "@/app/projects/page";
 import DarkButton from "@/app/components/button/darkButton/page";
 
 export default function Projects() {
   return (
-    <>
-      <div id="projects" className={styles.container}>
-        <div className={styles.subContainer}>
-          <SmallText>Selected work</SmallText>
-        </div>
-        <ProjectList />
-        <DarkButton>All projects</DarkButton>
+    <div className={styles.container}>
+      <div className={styles.subContainer}>
+        <SmallText>Selected work</SmallText>
       </div>
-    </>
+      <div className={styles.items}>
+        {data.map((item) => (
+          <div key={item.id} className={styles.projectItem}>
+            <Link href={`/projects/${item.id}`}>
+              <div className={styles.link}>
+                <Image
+                  src={item.image}
+                  alt={item.heading}
+                  width={300}
+                  height={200}
+                  layout="responsive"
+                  className={styles.projectImage}
+                  />
+              </div>
+            </Link>
+            <div className={styles.heading}>
+              <Link href={`/projects/${item.id}`} className={styles.link}>
+                <SpanText>{item.heading}</SpanText>
+              </Link>
+            </div>
+            <Line />
+            <Link href={`/projects/${item.id}`} className={styles.link}>
+              <ParagraphText>{item.subheading}</ParagraphText>
+            </Link>
+          </div>
+        ))}
+      </div>
+      <Link href={`/projects`} className={styles.button}>
+        <DarkButton>All projects</DarkButton>
+      </Link>
+    </div>
   );
 }
