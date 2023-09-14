0b60f465c01078c114cce81a6b70c0905c6fca5e
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-14 12:27:43 +0200

    Implement data to projects page w/ updated styling

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,40 +1,47 @@
-import Link from "next/link";
-import data from "@/app/utils/projectsdata";
-import Line from "../components/line/page";
-import SpanText from "../components/headings/spanText/page";
+import React from 'react';
+import Link from 'next/link';
+import DarkButton from "../components/button/darkButton/page";
 import ParagraphText from "../components/headings/paragraphText/page";
+import SmallText from "../components/headings/smallText/page";
+import SpanText from "../components/headings/spanText/page";
+import Line from "../components/line/page";
 import styles from "./projects.module.css";
+import data from "@/app/utils/projectsdata";
 
 export default function Projects() {
   return (
     <div className={styles.container}>
-      <div className={styles.line}>
-        <Line />
-      </div>
-      <div className={styles.items}>
-        {data.map((item) => (
-          <div key={item.id} className={styles.projectItem}>
-            <Link href={`/projects/${item.id}`} className={styles.link}>
-              <div className={styles.heading}>
-                <SpanText>{item.heading}</SpanText>
-              </div>
-            </Link>
-            <Link href={`/projects/${item.id}`} className={styles.link}>
-              <div className={styles.projectInfo}>
-                <div className={styles.columnDeliverables}>
-                  <ParagraphText>{item.deliverables}</ParagraphText>
-                </div>
-                <div className={styles.columnClient}>
-                  <ParagraphText>{item.client}</ParagraphText>
-                </div>
-                <div className={styles.columnYear}>
-                  <ParagraphText>{item.year}</ParagraphText>
-                </div>
-              </div>
-            </Link>
+      <div className={styles.subTitle}>All projects</div>
+      <span className={styles.contentTitle}>
+        <SmallText className={styles.columnOne}>Client</SmallText>
+        <SmallText className={styles.columnTwo}>Deliverables</SmallText>
+        <SmallText className={styles.columnThree}>Location</SmallText>
+        <SmallText className={styles.columnFour}>Year</SmallText>
+        <div className={styles.contentDesc}></div>
+      </span>
+      <Line />
+      {data.map((item, index) => (
+        <React.Fragment key={item.id}>
+          {index > 0 && <Line />}
+          <div className={styles.contentProject}>
+            {item.link ? (
+              <Link href={item.link} className={styles.link}>
+                <SpanText className={styles.contentClient}>{item.client}</SpanText>
+              </Link>
+            ) : (
+              <SpanText className={styles.contentClient}>{item.client}</SpanText>
+            )}
+            <ParagraphText className={styles.contentDel}>{item.deliverables}</ParagraphText>
+            <ParagraphText className={styles.contentLoc}>{item.location}</ParagraphText>
+            <ParagraphText className={styles.contentYear}>{item.year}</ParagraphText>
           </div>
-        ))}
-      </div>
+        </React.Fragment>
+      ))}
+      <Line />
+      {/* Archive Button */}
+      <Link href={`/archive`} className={styles.button}>
+        <DarkButton>Archive</DarkButton>
+      </Link>
     </div>
   );
 }
