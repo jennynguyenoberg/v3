4398a568ffa6b8b96e8983d56fbb27341ea474a8
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-15 15:32:27 +0200

    Update styling 404-page

--- app/[...slug]/page.js
+++ app/[...slug]/page.js
@@ -1,14 +1,16 @@
 import styles from './notfound.module.css';
 import DarkLinkItem from '../components/link/darkLink/page';
+import SpanText from '../components/headings/spanText/page';
+import ParagraphText from '../components/headings/paragraphText/page';
 
 export default function NotFoundPage() {
   return (
     <>
       <div className={styles.container}>
-        <span className={styles.subtitle}>Oops...</span>
-        <span className={styles.title}>404</span>
-        <p>That page cannot be found.</p>
-        <p>Go back to the <DarkLinkItem href="/">Homepage</DarkLinkItem></p>
+        <SpanText className={styles.subtitle}>Oops...</SpanText> 
+        <p className={styles.title}>404</p>
+        <SpanText>That page cannot be found.</SpanText>
+        <ParagraphText className={styles.subtitle}>Go back to the <DarkLinkItem href="/">Homepage</DarkLinkItem></ParagraphText>
       </div>
     </>
   );
