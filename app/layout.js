ea46c885c6d172fcb468fb3bc6bb6741d120793b
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-15 16:49:18 +0200

    Add text highlight function

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,50 @@
+"use client";
 import "../styles/globals.css";
 import Head from "next/head";
 import Navigation from "./components/navigation/page";
 import Footer from "./components/footer/page";
 import Contact from "./sections/contact/page";
-
-export const metadata = {
-  title: "Jenny Nguyen Öberg — Designer & Developer",
-  description: "Portfolio of Jenny Nguyen Öberg",
-};
+import React, { useEffect, useState } from "react";
 
 export default function RootLayout({ children }) {
+  const metadata = {
+    title: "Jenny Nguyen Öberg — Designer & Developer",
+    description: "Portfolio of Jenny Nguyen Öberg",
+  };
+  
+  const [highlightColor, setHighlightColor] = useState("#8b80e5");
+  
+  useEffect(() => {
+    const colors = ["#8b80e5", "#b7e0e6"];
+    const handleMouseDown = () => {
+      const color = colors.shift();
+      setHighlightColor(color);
+      colors.push(color);
+    };
+  
+    window.addEventListener("mousedown", handleMouseDown);
+  
+    return () => {
+      window.removeEventListener("mousedown", handleMouseDown);
+    };
+  }, []);
+
+
   return (
     <html lang="en">
       <Head>
         <title>{metadata.title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content={metadata.description} />
       </Head>
       <body>
-        <Navigation />
-        {children}
-        <Contact />
-        <Footer />
+        <div style={{ "--highlight-color": highlightColor }}>
+          <Navigation />
+          {children}
+          <Contact />
+          <Footer />
+        </div>
       </body>
     </html>
   );
