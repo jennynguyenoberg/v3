23e50bff6b1480410d27fe1f73e463332dae000e
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2024-02-20 10:55:15 +0100

    Change alert text

--- app/sections/hero/page.js
+++ app/sections/hero/page.js
@@ -1,20 +1,23 @@
 import styles from "./hero.module.css";
-import SubHeadingRotate from "@/app/components/headings/subHeadingRotate/page";
 import TitleScroll from "@/app/components/headings/titleScroll/page";
 import SpanText from "@/app/components/headings/spanText/page";
+import DarkLinkItem from "@/app/components/link/darkLink/page";
+import classNames from "classnames";
 
 export default function Hero() {
   return (
     <div className={styles.container}>
       <div className={styles.rowWrapper}>
         <div className={styles.calloutText}>
-          ↳ Looking for an internship starting in
+          ↳ Current internship at <DarkLinkItem
+            href="https://getbower.com/"
+            className={classNames(styles.linkItem)}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Bower
+          </DarkLinkItem>
         </div>
-        <SubHeadingRotate>
-          <span>January</span>
-          <br />
-          <span>2024</span>
-        </SubHeadingRotate>
       </div>
       <TitleScroll />
       <div className={styles.spanContainer}>
@@ -23,3 +26,29 @@ export default function Hero() {
     </div>
   );
 }
+
+// import styles from "./hero.module.css";
+// import SubHeadingRotate from "@/app/components/headings/subHeadingRotate/page";
+// import TitleScroll from "@/app/components/headings/titleScroll/page";
+// import SpanText from "@/app/components/headings/spanText/page";
+
+// export default function Hero() {
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.rowWrapper}>
+//         <div className={styles.calloutText}>
+//           ↳ Looking for an internship starting in
+//         </div>
+//         <SubHeadingRotate>
+//           <span>January</span>
+//           <br />
+//           <span>2024</span>
+//         </SubHeadingRotate>
+//       </div>
+//       <TitleScroll />
+//       <div className={styles.spanContainer}>
+//         <SpanText>Designer & Developer</SpanText>
+//       </div>
+//     </div>
+//   );
+// }
